1c9dc2c1655f275c60c17f944ace9311f6fecad6
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-07-30 12:39:52 +0200

    Add fix stop add empty list element to add task function

--- js/script.js
+++ js/script.js
@@ -55,27 +55,28 @@ function addTask(event) {
   event.preventDefault();
   if(taskInput.value === '') {
     alert('Add a task');
+  } else {
+    //create li element
+    const li = document.createElement('li');
+    //add class
+    li.className = 'collection-item';
+    //create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    //create new link element
+    const link = document.createElement('a');
+    //add class
+    link.className = 'delete-item secondary-content';
+    //add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //append the link to li
+    li.appendChild(link);
+    //append li to ul
+    taskList.appendChild(li);
+    //save in local storage
+    storeTaskInLocalStorage(taskInput.value);
+    //clear input
+    taskInput.value = '';
   }
-  //create li element
-  const li = document.createElement('li');
-  //add class
-  li.className = 'collection-item';
-  //create text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
-  //create new link element
-  const link = document.createElement('a');
-  //add class
-  link.className = 'delete-item secondary-content';
-  //add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  //append the link to li
-  li.appendChild(link);
-  //append li to ul
-  taskList.appendChild(li);
-  //save in local storage
-  storeTaskInLocalStorage(taskInput.value);
-  //clear input
-  taskInput.value = '';
 }
 //store task
 function storeTaskInLocalStorage(task) {
